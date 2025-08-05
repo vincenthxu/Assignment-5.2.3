06a8e70d5dbe93ffe409587e4d812816c9539cf2
Vincent Xu <vincent.xu1@outlook.com>
2025-08-05 13:38:38 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment_5._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EnumerateNums(10);
+            Console.WriteLine();
+        }
+
+        static void EnumerateNums(int num)
+        {
+            Console.Write(num + " ");
+            if (num > 1)
+                EnumerateNums(num - 1);
+        }
+    }
+}
